85625878f40a08f937b9e328d40c1aa5d6fab842
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-11 05:25:23 -0800

    complete

--- while_repeat_While/main.swift
+++ while_repeat_While/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
 
+var  i = 1
+while i<10 {
+    print("the value of i*i is \(i*i)!")
+    i+=1
+}
+
+var number = 981289
+var temp = number
+var palin = 0
+
+while temp > 0
+{
+    
+    let digit = temp % 10
+    //print(digit)
+    palin = palin * 10 + (digit)
+    
+    temp = temp / 10
+    
+}
+
+
+if palin == number{
+    print("your number is palindrom number!")
+}
+else{
+    print("your number is not palindrom!")
+}
+/*
+repeat{
+    
+}while(true)
+*/
